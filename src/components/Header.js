bbf861d33fef93d512b2106aeee49c13d2a84eb9
Romesh <rosin@digitalyogin.com>
2022-05-28 13:35:22 +0200

    Add files via upload
    Adding files

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+import { PageHeader, Button, Input, Space, Badge  } from 'antd';
+import { useMoralis } from "react-moralis";
+import { Link } from 'react-router-dom';
+import './Header.css'
+import Amazon from "../images/logo.png";
+import USA from "../images/usa.png";
+import BookStore from "../images/bookstore.png";
+import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+
+
+const {Search} = Input;
+const categories = ["Per genitori", "Bambini 0 a 5 anni", "Crescita personale", "Scuola primaria", "Genitorialità", "Adolescenti"];
+
+const Header = () => {
+  const { authenticate } = useMoralis();
+  return(
+    <div className="site-page-header-ghost-wrapper">
+      <PageHeader
+        ghost={false}
+        title="Tanta Roba"
+        extra={[
+          <>
+          <img src= {Amazon} className="logo"></img>
+          <img src={BookStore} className="logo"></img>
+          <Search
+              placeholder="Find A Product"
+              enterButton
+              className = "searchBar"
+            />
+         <Button 
+         className="login"
+         key="1" 
+         type="primary" 
+         onClick={() => authenticate()}>
+          Login with Metamask
+          </Button>
+          <Space size={"large"}>
+              
+              <Badge count={0} showZero>
+                <span className="header-buttons">
+                  <ShoppingCartOutlined className="header-icon" />
+                  Carrello
+                </span>
+              </Badge>
+              <Space className="header-buttons" size={"small"}>
+                <img src={USA} alt="region" className="flag"></img>▾
+              </Space>
+              
+            </Space>
+          </>
+        ]}>
+      </PageHeader>
+      <div className="site-page-subheader-ghost-wrapper">
+      <Space size={"middle"}>
+        <Space size={"small"} style={{fontWeight:"bold"}}>
+          <MenuOutlined />
+          Categorie
+        </Space>
+        {categories.map((e) =>{
+          return(
+            <Link to="/categories" state={e} className="categories">
+              {e}
+            </Link>
+          )
+
+        })}
+      </Space>
+    </div>
+    </div>
+  )
+}
+
+export default Header;
